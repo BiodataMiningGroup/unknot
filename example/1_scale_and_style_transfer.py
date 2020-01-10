32d83749f29766bacc1d759a33165a19ae93c643
Martin Zurowietz <martin@zurowietz.de>
2020-01-10 12:38:49 +0100

    Simplify example script 1 number of workers

--- example/1_scale_and_style_transfer.py
+++ example/1_scale_and_style_transfer.py
@@ -6,7 +6,10 @@ from src.Dataset import Dataset
 source = Dataset(sys.argv[1])
 target = Dataset(sys.argv[2])
 
+# Get number of usable CPUs.
+workers = len(os.sched_getaffinity(0))
+
 # Torch and TensorFlow can't be executed in the same script so style transfer and object
 # detection have to be split into two separate scripts.
-annotation_patches = source.generate_annotation_patches(scale_transfer_target=target, max_workers=10)
-annotation_patches = target.apply_style_transfer(annotation_patches, device='cuda', max_workers=10, steps=10000)
+annotation_patches = source.generate_annotation_patches(scale_transfer_target=target, max_workers=workers)
+annotation_patches = target.apply_style_transfer(annotation_patches, device='cuda', max_workers=workers, steps=10000)
